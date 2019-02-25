50e67f9091cf72d249df3cecbc9581dd10e6ab4b
mwsmws22 <mitchellwsmith@hotmail.com>
2019-02-25 13:36:12 -0600

    FINAL

--- student_code_game_masters.py
+++ student_code_game_masters.py
@@ -73,20 +73,20 @@ class TowerOfHanoiGame(GameMaster):
 
         self.kb.kb_retract(Fact(Statement(['top', movable_statement.terms[0], movable_statement.terms[1]])))
 
-        if new_peg:
-            self.kb.kb_retract(Fact(Statement(['top', 'disk' + str(new_peg[0]), movable_statement.terms[2]])))
-        else:
-            self.kb.kb_retract(Fact(Statement(['empty', movable_statement.terms[2]])))
-
-        self.kb.kb_assert(Fact(Statement(['on', movable_statement.terms[0], movable_statement.terms[2]])))
-        self.kb.kb_assert(Fact(Statement(['top', movable_statement.terms[0], movable_statement.terms[2]])))
-
         if old_peg:
             self.kb.kb_assert(Fact(Statement(['top', 'disk' + str(old_peg[0]), movable_statement.terms[1]])))
         else:
             self.kb.kb_assert(Fact(Statement(['empty', movable_statement.terms[1]])))
 
+        self.kb.kb_assert(Fact(Statement(['on', movable_statement.terms[0], movable_statement.terms[2]])))
         self.kb.kb_retract(Fact(Statement(['on', movable_statement.terms[0], movable_statement.terms[1]])))
+
+        if new_peg:
+            self.kb.kb_retract(Fact(Statement(['top', 'disk' + str(new_peg[0]), movable_statement.terms[2]])))
+        else:
+            self.kb.kb_retract(Fact(Statement(['empty', movable_statement.terms[2]])))
+
+        self.kb.kb_assert(Fact(Statement(['top', movable_statement.terms[0], movable_statement.terms[2]])))
 
         return
 
